bbb2197a264f222e02f32e5d42b92dce7a40542a
Arjun Chand <124900252+arjun-chand@users.noreply.github.com>
2023-11-24 19:08:00 +0530

    Update BlogService.java

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -6,6 +6,7 @@ import com.driver.models.User;
 import com.driver.repositories.BlogRepository;
 import com.driver.repositories.ImageRepository;
 import com.driver.repositories.UserRepository;
+import org.hibernate.query.criteria.internal.expression.function.CurrentTimestampFunction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,13 +22,30 @@ public class BlogService {
     @Autowired
     UserRepository userRepository1;
 
+    @Autowired
+    ImageService imageService;
+
     public Blog createAndReturnBlog(Integer userId, String title, String content) {
         //create a blog at the current time
+        Blog blog = new Blog();
+        blog.setTitle(title);
+        blog.setContent(content);
+        blog.setPubDate(new Date());
 
+        User user = userRepository1.findById(userId).orElse(null);
+        blog.setUser(user);
+
+        blogRepository1.save(blog);
+        return blog;
     }
 
     public void deleteBlog(int blogId){
         //delete blog and corresponding images
+        blogRepository1.deleteById(blogId);
 
+    }
+    public void addImage(Integer blogId, String description, String dimension){
+        Blog blog = blogRepository1.findById(blogId).orElse(null);
+        imageService.addImage(blog.getId(),description,dimension);
     }
 }
