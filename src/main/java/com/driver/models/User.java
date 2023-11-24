bc3947f43ffa13d3f27f5bfb35da298273af893f
Arjun Chand <124900252+arjun-chand@users.noreply.github.com>
2023-11-24 19:06:08 +0530

    Update User.java

---
+++ src/main/java/com/driver/models/User.java
@@ -0,0 +1,68 @@
+
+package com.driver.models;
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String userName;
+    private String firstName;
+    private String LastName;
+    private String  password;
+    @OneToMany
+    List<Blog> blogs;
+
+    public User() {
+    }
+
+    public User(int id, String userName, String firstName, String lastName, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.firstName = firstName;
+        LastName = lastName;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
